1f61c2710917facd27fe1c5da8a2efc20f521361
benr-ml <benriva@mystenlabs.com>
2022-11-02 19:11:39 +0200

    basic bench

---
+++ crates/threshold-bls/benches/benchmark.rs
@@ -0,0 +1,18 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use threshold_bls::sig::test_threshold_g1;
+use std::time::Duration;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut t: usize = 128;
+    while t < 129 {
+        c.bench_function(format!("tbls {}", t).as_str(), |b| b.iter(|| test_threshold_g1(t)));
+        t *= 2;
+    }
+}
+
+criterion_group!{
+  name = benches;
+  config = Criterion::default().measurement_time(Duration::from_secs(10));
+  targets = criterion_benchmark
+}
+criterion_main!(benches);
